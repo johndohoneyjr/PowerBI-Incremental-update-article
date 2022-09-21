19a40efd796cb8bc914b0355e5db96ebb17e6fcc
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2022-09-21 16:43:45 -0700

    Fix PBIX file

--- Cosmos Provisioning/nukeDataBase.sh
+++ Cosmos Provisioning/nukeDataBase.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export randomIdentifier="572838462"
+export randomIdentifier="552189312"
 resourceGroup="msdocs-cosmosdb-rg-$randomIdentifier"
 
 az group delete --name $resourceGroup
